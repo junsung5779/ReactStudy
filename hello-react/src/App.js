8b0a45ab475cb906ef66cf2bc110b2e194672739
Junsung Choi <junsung5779@gmail.com>
2023-01-03 21:01:07 +0900

    1일차

---
+++ hello-react/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  const name = "react";
+  return (
+    <div>
+      <h1 className="react">{name} 안녕</h1>
+      <h2>잘 작동하니?</h2>
+    </div>
+  );
+}
+
+export default App;
