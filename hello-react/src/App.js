025493426349ce6df5aba229cd7c896c8b04079d
Junsung Choi <junsung5779@gmail.com>
2023-01-03 22:26:50 +0900

    ~export & import module

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,13 +1,10 @@
-import "./App.css";
+import MyComponent from './myComponent';
 
-function App() {
-  const name = "react";
-  return (
-    <div>
-      <h1 className="react">{name} 안녕</h1>
-      <h2>잘 작동하니?</h2>
-    </div>
-  );
-}
-
+const App = () => {
+	return (
+		<div>
+			<MyComponent />
+		</div>
+	);
+};
 export default App;
---
+++ hello-react/src/MyComponent.js
@@ -0,0 +1,4 @@
+const MyComponent = () => {
+	return <div>나의 새롭고 멋진 컴포넌트</div>;
+};
+export default MyComponent;
