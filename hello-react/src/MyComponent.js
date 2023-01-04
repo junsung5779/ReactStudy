278fa15e79d509cfda248bb4f8928f7162510d62
Junsung Choi <junsung5779@gmail.com>
2023-01-04 20:08:04 +0900

    class 내부에서 defaultProps, propTypes 지정

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,12 +1,19 @@
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Component } from 'react';
 
 class MyComponent extends Component {
+	static defaultProps = {
+		name: '기본 이름',
+	};
+	static propTypes = {
+		name: PropTypes.string,
+		favoriteNumber: PropTypes.number.isRequired,
+	};
 	render() {
 		const { name, favoriteNumber, children } = this.props; // 비구조화 할당
 		return (
 			<div>
-				안녕하세요, 제 이름은 {name} 입니1111다. <br />
+				안녕하세요, 제 이름은 {name} 입니다. <br />
 				children 값은 {children}
 				입니다.
 				<br />
