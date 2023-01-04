1893970cf3e0d9cb5a702a70d3072c9fe5cacfba
Junsung Choi <junsung5779@gmail.com>
2023-01-04 21:38:29 +0900

    한 컴포넌트에서 useState 여러번 사용하는법

--- hello-react/src/Say.js
+++ hello-react/src/Say.js
@@ -10,11 +10,25 @@ const Say = () => {
 	const onClickEnter = () => setMessage('안녕하세요!');
 	const onClickLeave = () => setMessage('안녕히 가세요!');
 
+	const [color, setColor] = useState('black');
+
 	return (
 		<div>
 			<button onClick={onClickEnter}>입장</button>
 			<button onClick={onClickLeave}>퇴장</button>
-			<h1>{message}</h1>
+			<h1 style={{ color }}>{message}</h1>
+			<button style={{ color: 'red' }} onClick={() => setColor('red')}>
+				빨간색
+			</button>
+			<button
+				style={{ color: 'green' }}
+				onClick={() => setColor('green')}
+			>
+				초록색
+			</button>
+			<button style={{ color: 'blue' }} onClick={() => setColor('blue')}>
+				파란색
+			</button>
 		</div>
 	);
 };
