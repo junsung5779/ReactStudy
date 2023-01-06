bd2287f0986aa166d32acedcf0540e7ff96706ef
Junsung Choi <junsung5779@gmail.com>
2023-01-06 22:14:38 +0900

    event 객체를 활용하여 input 여러 개 다루기

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 class EventPractice extends Component {
 	state = {
+		username: '',
 		message: '',
 	};
 
@@ -11,13 +12,14 @@ class EventPractice extends Component {
 	 */
 	handleChange = (e) => {
 		this.setState({
-			message: e.target.value,
+			[e.target.name]: e.target.value,
 		});
 	};
 
 	handleClick = () => {
-		alert(this.state.message);
+		alert(this.state.username + ': ' + this.state.message);
 		this.setState({
+			username: '',
 			message: '',
 		});
 	};
@@ -26,6 +28,13 @@ class EventPractice extends Component {
 		return (
 			<div>
 				<h1>이벤트 연습</h1>
+				<input
+					type="text"
+					name="username"
+					placeholder="사용자명"
+					value={this.state.username}
+					onChange={this.handleChange}
+				/>
 				<input
 					type="text"
 					name="message"
