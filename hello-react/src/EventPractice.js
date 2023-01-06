c0606582303c18b0031a321c614f4043f5f4a9b1
Junsung Choi <junsung5779@gmail.com>
2023-01-06 22:56:38 +0900

    함수 컴포넌트로 이벤트 핸들링 구현

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -1,59 +1,54 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class EventPractice extends Component {
-	state = {
+const EventPractice = () => {
+	const [form, setForm] = useState({
 		username: '',
 		message: '',
+	});
+
+	const { username, message } = form;
+	const onChange = (e) => {
+		const nextForm = {
+			...form, // 기존의 form 내용을 이 자리에 복사한 뒤
+			[e.target.name]: e.target.value, //원하는 값을 덮어 씌우기
+		};
+		setForm(nextForm);
 	};
 
-	/**
-	 * 바벨의 transform-class-properties 문법을 사용하여
-	 * 화살표 함수 형태로 메서드 정의하여 메서드 바인딩을 간단하게 하였다.
-	 */
-	handleChange = (e) => {
-		this.setState({
-			[e.target.name]: e.target.value,
-		});
-	};
-
-	handleClick = () => {
-		alert(this.state.username + ': ' + this.state.message);
-		this.setState({
+	const onClick = () => {
+		alert(username + ': ' + message);
+		setForm({
 			username: '',
 			message: '',
 		});
 	};
-
-	handleKeyDown = (e) => {
-		// 누른 키가 Enter 키 일 경우 이 컴포넌트에서 선언한 handleClick()함수 실행
+	const onKeyDown = (e) => {
 		if (e.key === 'Enter') {
-			this.handleClick();
+			onClick();
 		}
 	};
 
-	render() {
-		return (
-			<div>
-				<h1>이벤트 연습</h1>
-				<input
-					type="text"
-					name="username"
-					placeholder="사용자명"
-					value={this.state.username}
-					onChange={this.handleChange}
-				/>
-				<input
-					type="text"
-					name="message"
-					placeholder="아무거나 입력해 보세요"
-					value={this.state.message}
-					onChange={this.handleChange}
-					onKeyDown={this.handleKeyDown}
-				/>
-				<button onClick={this.handleClick}>확인</button>
-			</div>
-		);
-	}
-}
+	return (
+		<div>
+			<h1>이벤트 연습</h1>
+			<input
+				type="text"
+				name="username"
+				placeholder="사용자명"
+				value={username}
+				onChange={onChange}
+			/>
+			<input
+				type="text"
+				name="message"
+				placeholder="아무거나 입력해 보세요"
+				value={message}
+				onChange={onChange}
+				onKeyDown={onKeyDown}
+			/>
+			<button onClick={onClick}>확인</button>
+		</div>
+	);
+};
 
 export default EventPractice;
