f110ca5bdd942309491c9f2f5865b855ecfd81ef
Junsung Choi <junsung5779@gmail.com>
2023-01-06 21:55:51 +0900

    더 간단한 메서드 바인딩 방법

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -5,32 +5,22 @@ class EventPractice extends Component {
 		message: '',
 	};
 
-	constructor(props) {
-		super(props);
-
-		/**
-		 * 함수가 호출될 때 this는 호출부에 따라 결정되므로, 클래스의 임의 메서드가 특정 HTML 요소의
-		 * 이벤트로 등록되는 과정에서 메서드와 this의 관계가 끊어진다.
-		 * 이 때문에 임의 메서드가 이벤트로 등록되어도 this가 컴포넌트 자신으로 제대로 가리키기 위해서는
-		 * 메서드를 this와 바인딩 하는 작업이 필요하다.
-		 * 바인딩 작업을 하지 않을 경우 this가 undefined를 가리킨다.
-		 */
-		this.handleChange = this.handleChange.bind(this);
-		this.handleClick = this.handleClick.bind(this);
-	}
-
-	handleChange(e) {
+	/**
+	 * 바벨의 transform-class-properties 문법을 사용하여
+	 * 화살표 함수 형태로 메서드 정의하여 메서드 바인딩을 간단하게 하였다.
+	 */
+	handleChange = (e) => {
 		this.setState({
 			message: e.target.value,
 		});
-	}
+	};
 
-	handleClick() {
+	handleClick = () => {
 		alert(this.state.message);
 		this.setState({
 			message: '',
 		});
-	}
+	};
 
 	render() {
 		return (
