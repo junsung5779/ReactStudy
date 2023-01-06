5c4051b49422a9e02ef01d767747b60804f06df0
Junsung Choi <junsung5779@gmail.com>
2023-01-06 20:59:55 +0900

    메서드 바인딩

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -4,6 +4,34 @@ class EventPractice extends Component {
 	state = {
 		message: '',
 	};
+
+	constructor(props) {
+		super(props);
+
+		/**
+		 * 함수가 호출될 때 this는 호출부에 따라 결정되므로, 클래스의 임의 메서드가 특정 HTML 요소의
+		 * 이벤트로 등록되는 과정에서 메서드와 this의 관계가 끊어진다.
+		 * 이 때문에 임의 메서드가 이벤트로 등록되어도 this가 컴포넌트 자신으로 제대로 가리키기 위해서는
+		 * 메서드를 this와 바인딩 하는 작업이 필요하다.
+		 * 바인딩 작업을 하지 않을 경우 this가 undefined를 가리킨다.
+		 */
+		this.handleChange = this.handleChange.bind(this);
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleChange(e) {
+		this.setState({
+			message: e.target.value,
+		});
+	}
+
+	handleClick() {
+		alert(this.state.message);
+		this.setState({
+			message: '',
+		});
+	}
+
 	render() {
 		return (
 			<div>
@@ -13,16 +41,9 @@ class EventPractice extends Component {
 					name="message"
 					placeholder="아무거나 입력해 보세요"
 					value={this.state.message}
-					onChange={(e) => {
-						/**
-						 * e객체는 SyntheticEvent로 웹 브라우저의 네이티브 이벤트를 감싸는 객체
-						 * 네이티브 이벤트와 인터페이스가 같다.
-						 * 단, SyntheticEvent는 이벤트가 끝나고 나면 이벤트가 초기화된다.
-						 * 비동기적으로 이벤트 객체를 참조할 일이 있다면 e.persist() 함수를 호출해 주어야 한다.
-						 */
-						console.log(e);
-					}}
+					onChange={this.handleChange}
 				/>
+				<button onClick={this.handleClick}>확인</button>
 			</div>
 		);
 	}
