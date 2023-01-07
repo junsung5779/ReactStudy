5e53d15ce33dcee6ca590a8ebf58079647e06c07
Junsung Choi <junsung5779@gmail.com>
2023-01-07 16:18:03 +0900

    효과적인 리렌더링을 위한 key값 사용

--- hello-react/src/IterationSample.js
+++ hello-react/src/IterationSample.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 const IterationSample = () => {
 	const names = ['눈사람', '얼음', '눈', '바람'];
-	const nameList = names.map((name) => <li>{name}</li>);
+	/**
+	 * 고유한 값이 없을 때만 index 값을 key로 사용해야 한다.
+	 * key 값은 언제나 유잃야 한다. 따라서 데이터가 가진 고윳값을 key 값으로 설정해야 한다.
+	 * index를 key로 사용하면 배열이 변경될 때 효율적으로 리렌더링하지 못함
+	 */
+	const nameList = names.map((name, index) => <li key={index}>{name}</li>);
 
 	return <ul>{nameList}</ul>;
 };
