36fe839b9b1f0b8ee2f81304b2ee3f0edd53c1a6
Junsung Choi <junsung5779@gmail.com>
2023-01-07 15:59:32 +0900

    컴포넌트에 ref 달고 내부 메서드 사용

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
-import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox';
 
 class App extends Component {
 	render() {
-		return <ValidationSample />;
+		return (
+			<div>
+				<ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+				<button onClick={() => this.scrollBox.scrollToBottom()}>
+					맨 밑으로 스크롤 이동
+				</button>
+			</div>
+		);
 	}
 }
 
