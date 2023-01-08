54140c3ec7638eb03059f4d1d616ea79a1490375
Junsung Choi <junsung5779@gmail.com>
2023-01-08 15:52:14 +0900

    how to use lifecycle method

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -6,11 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 // id가 'root'인 요소에 리액트 컴포넌트를 렌더링
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
