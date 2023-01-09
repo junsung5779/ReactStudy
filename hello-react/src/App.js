889c353741b0d5a48f9336ff63ba421751bdf1ea
Junsung Choi <junsung5779@gmail.com>
2023-01-09 21:48:39 +0900

    useReducer 개념

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,20 +1,6 @@
-import React, { useState } from 'react';
 import Info from './info';
 
 const App = () => {
-	const [visible, setVisible] = useState(false);
-	return (
-		<div>
-			<button
-				onClick={() => {
-					setVisible(!visible);
-				}}
-			>
-				{visible ? '숨기기' : '보이기'}
-			</button>
-			<hr />
-			{visible && <Info />}
-		</div>
-	);
+	return <Info />;
 };
 export default App;
--- hello-react/src/info.js
+++ hello-react/src/info.js
@@ -1,43 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useReducer } from 'react';
 
-const Info = () => {
-	const [name, setName] = useState('');
-	const [nickname, setNickname] = useState('');
-	useEffect(() => {
-		console.log('짜란~ 마운트될 때만 실행됩니다~');
-		console.log(name);
-		/**
-		 * useEffect는 기본적으로 렌더링되고 난 직후마다 실행되며,
-		 * 두 번째 파라미터 배열에 무엇을 넣는지에 따라 실행되는 조건이 달라진다.
-		 *
-		 * useEffect의 두 번째 파라미터로 전달되는 배열 안에 검사하고 싶은 값을 넣어주면,
-		 * 해당 value가 바뀔 때만 useEffect() 함수가 실행된다.
-		 *
-		 * 배열 안에는 useState를 통해 관리하고 있는 상태를 넣어 주어도 되고,
-		 * props로 전달받은 값을 넣어 주어도 된다.
-		 *
-		 * 컴포넌트가 언마운ㄴ트되기 전이나 업데이트되기 직전에 어떠한 작업을 수행하고 싶다면
-		 * useEffect에서 cleanup 함수(걍 함수 리턴 ㅇㅇ..)를 반환해 주어야 한다.
-		 */
-		return () => {
-			console.log('cleanup');
-			console.log(name);
-		};
-	}, [name]);
-
-	const onChangeName = (e) => {
-		setName(e.target.value);
+function reducer(state, action) {
+	return {
+		...state,
+		[action.name]: action.value,
 	};
-
-	const onChangeNickname = (e) => {
-		setNickname(e.target.value);
+}
+const Info = () => {
+	const [state, dispatch] = useReducer(reducer, {
+		name: '',
+		nickname: '',
+	});
+	const { name, nickname } = state;
+	const onChange = (e) => {
+		/**
+		 * useReducer에서의 액션은 그 어떤 값도 사용 가능하기 때문에
+		 * 이벤트 객체가 지니고 있는 e.target값 자체를 액션 값으로 사용하여
+		 * 아무리 인풋의 개수가 많아져도 코드를 짧고 깔끔하게 유지할 수 있다.
+		 */
+		dispatch(e.target);
 	};
 
 	return (
 		<div>
 			<div>
-				<input value={name} onChange={onChangeName} />
-				<input value={nickname} onChange={onChangeNickname} />
+				<input name="name" value={name} onChange={onChange} />
+				<input name="nickname" value={nickname} onChange={onChange} />
 			</div>
 			<div>
 				<div>
