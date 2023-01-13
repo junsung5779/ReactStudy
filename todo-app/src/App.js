c3ab3a33a6276ae1bcfca14d285743cab7989e0e
Junsung Choi <junsung5779@gmail.com>
2023-01-13 21:17:18 +0900

    기본 페이지 및 UI 구성

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,5 +1,7 @@
+import TodoTemplate from './components/TodoTemplate';
+
 const App = () => {
-  return <div>Todo App을 만들자!</div>;
+  return <TodoTemplate>Todo App을 만들자!</TodoTemplate>;
 };
 
 export default App;
