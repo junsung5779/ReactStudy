4a0f9cd07b3bc297e5d79494232bf31b555eb19a
Junsung Choi <junsung5779@gmail.com>
2023-01-13 22:03:37 +0900

    TodoComponent Styling

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,12 +1,14 @@
 import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
 const App = () => {
-  return (
-    <TodoTemplate>
-      <TodoInsert />
-    </TodoTemplate>
-  );
+    return (
+        <TodoTemplate>
+            <TodoInsert />
+            <TodoList />
+        </TodoTemplate>
+    );
 };
 
 export default App;
--- todo-app/src/components/TodoInsert.js
+++ todo-app/src/components/TodoInsert.js
@@ -2,13 +2,13 @@ import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
 const TodoInsert = () => {
-  return (
-    <form className="TodoInsert">
-      <input placeholder="할 일을 입력하세요" />
-      <button type="submit">
-        <MdAdd />
-      </button>
-    </form>
-  );
+    return (
+        <form className="TodoInsert">
+            <input placeholder="할 일을 입력하세요" />
+            <button type="submit">
+                <MdAdd />
+            </button>
+        </form>
+    );
 };
 export default TodoInsert;
