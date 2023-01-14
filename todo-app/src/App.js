46be42e94a51273753e9eaa2bbe99485751c0158
Junsung Choi <junsung5779@gmail.com>
2023-01-14 16:22:59 +0900

    React.memo를 사용하여 성능 최적화

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -3,24 +3,20 @@ import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
-const App = () => {
-    const [todos, setTodos] = useState([
-        {
-            id: 1,
-            text: '리액트의 기초 알아보기',
-            checked: true,
-        },
-        {
-            id: 2,
-            text: '컴포넌트 스타일링해 보기',
-            checked: true,
-        },
-        {
-            id: 3,
-            text: '일정 관리 앱 만들어 보기',
+function createBulkTools() {
+    const array = [];
+    for (let i = 1; i <= 2500; i++) {
+        array.push({
+            id: i,
+            text: `할 일 ${i}`,
             checked: false,
-        },
-    ]);
+        });
+    }
+    return array;
+}
+
+const App = () => {
+    const [todos, setTodos] = useState(createBulkTools);
     // 고윳값으로 사용될 id
     // ref를 사용하여 변수 담기
     const nextId = useRef(4);
