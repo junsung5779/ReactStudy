619d71eaab755200169397906bbf8d058f60c227
junsung5779 <junsung5779@gmail.com>
2023-12-05 00:52:19 +0900

    JSX 문법 3개(className, 데이터 바인딩, html에 스타일 직접 집어넣기)

--- coding-apple/src/App.js
+++ coding-apple/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  var data = "red";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>블로그</div>
+      </div>
+      <div className={data}>안녕하세요</div>
+      <div style={{ color: "blue", fontSize: "30px" }}> 글씨 </div>
     </div>
   );
 }
