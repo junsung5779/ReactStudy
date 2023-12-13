136b368a92a377f68014aed29c1b91eb170c6ce0
junsung5779 <junsung5779@gmail.com>
2023-12-13 22:00:05 +0900

    update: 따봉 data 변경

--- coding-apple/src/App.js
+++ coding-apple/src/App.js
@@ -3,25 +3,28 @@ import { useState } from "react";
 
 function App() {
   let [글제목, 글제목변경] = useState(["남자 코트 추천", "강남 우동맛집", "파이썬 독학"]);
-  let [따봉, 따봉변경] = useState(0);
+  let [따봉, 따봉변경] = useState([0, 0, 0]);
   return (
     <div className="App">
       <div className="black-nav">
         <div>React blog</div>
       </div>
 
+      <button>가나다순 정렬</button>
+
       <div className="list">
         <h4>
-          {" "}
-          {글제목[0]}{" "}
+          {글제목[0]}
           <span
             onClick={() => {
-              따봉변경(따봉 + 1);
+              let copy = [...따봉];
+              copy[0] += 1;
+              따봉변경(copy);
             }}
           >
             👍
-          </span>{" "}
-          {따봉}
+          </span>
+          {따봉[0]}
         </h4>
         <p>2월 17일 발행</p>
         <button
@@ -36,13 +39,33 @@ function App() {
       </div>
       <div className="list">
         <h4>
-          {글제목[1]} <span>👍</span>
+          {글제목[1]}{" "}
+          <span
+            onClick={() => {
+              let copy = [...따봉];
+              copy[1] += 1;
+              따봉변경(copy);
+            }}
+          >
+            👍
+          </span>
+          {따봉[1]}
         </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
         <h4>
-          {글제목[2]} <span>👍</span>
+          {글제목[2]}{" "}
+          <span
+            onClick={() => {
+              let copy = [...따봉];
+              copy[2] += 1;
+              따봉변경(copy);
+            }}
+          >
+            👍
+          </span>
+          {따봉[2]}
         </h4>
         <p>2월 17일 발행</p>
       </div>
