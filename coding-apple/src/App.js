c4afc2cd34de62fa6b957cfe41038e04d5e44402
하문_최준성 <juns5779@afreecatv.com>
2023-12-15 09:04:12 +0900

    study: 현재 누른 글 제목이 Modal창 안에 뜨게 하기

--- coding-apple/src/App.js
+++ coding-apple/src/App.js
@@ -5,6 +5,7 @@ function App() {
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬 독학']);
   let [따봉, 따봉변경] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
+  let [lastIdx, setLastIdx] = useState(0)
 
   return (
     <div className="App">
@@ -23,7 +24,10 @@ function App() {
       {글제목.map((a, i) => {
         return (
           <div className="list" key={i}>
-            <h4 onClick={()=>{setModal(!modal)}}>
+            <h4 onClick={()=>{
+                setLastIdx(i)
+                setModal(!modal)
+              }}>
               {글제목[i]}
               <span
                 onClick={() => {
@@ -39,7 +43,7 @@ function App() {
           </div>
         );
       })}
-      {modal === true ? <Modal 글제목={글제목} 글제목변경={글제목변경} /> : null}
+      {modal === true ? <Modal 글제목={글제목} 글제목변경={글제목변경} lastIdx={lastIdx} /> : null}
     </div>
   );
 
@@ -52,16 +56,10 @@ function App() {
   function Modal(props) {
     return (
       <div className="modal">
-        <h4>{props.글제목[0]}</h4>
+        <h4>{props.글제목[props.lastIdx]}</h4>
         <p>날짜</p>
         <p>상세내용</p>
-        <button onClick={()=>{
-            let copy = [...props.글제목];
-            copy[0] = '여자 코트 추천';
-            props.글제목변경(copy);
-            }
-          }>글 수정
-        </button>
+        <button>글 수정</button>
       </div>
     );
   }
