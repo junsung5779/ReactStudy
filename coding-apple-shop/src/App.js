b3c8e5ac6b9c7ec940837a93db4b6bba15cacc2f
junsung5779 <junsung5779@gmail.com>
2023-12-27 20:54:43 +0900

    study: useEffect2(사용조건 과 숙제)

--- coding-apple-shop/src/App.js
+++ coding-apple-shop/src/App.js
@@ -97,7 +97,7 @@ function App() {
 
   function Card(props) {
     return (
-      <div className="col-md-4" key={props.shoes.id}>
+      <div className="col-md-4" key={props.i}>
         <img src={`https://codingapple1.github.io/shop/shoes${props.i + 1}.jpg`} width="80%" alt="" />
         <h4>{props.shoes.title}</h4>
         <p>{props.shoes.content}</p>
--- coding-apple-shop/src/routes/Detail.js
+++ coding-apple-shop/src/routes/Detail.js
@@ -3,17 +3,36 @@ import { useParams } from "react-router-dom";
 
 function Detail(props) {
   let [alert, setAlert] = useState(true);
+  let [count, setCount] = useState(0);
+  let [input1, setInput1] = useState(0);
 
+  // Component mount(load), update, dependencies 실행조건을 충족시킬 때마다 useEffect 실행
   useEffect(() => {
-    // 여기적은 코드는 컴포넌트 load & update 마다 실행됨(라이프사이클 훅 과 동일한 개념)
+    // 여기적은 코드는 컴포넌트 mount(load) & update 마다 실행됨(라이프사이클 훅 과 동일한 개념)
     // 어려운 연산
     // 서버에서 데이터 가져오는 작업
     // 타이머
     console.log("useEffect hook은 html 요소들이 렌더링 되고난 후 실행됨");
-    setTimeout(() => {
+    let a = setTimeout(() => {
       setAlert(false);
     }, 2000);
-  });
+    // 숙제
+    if (isNaN(input1)) {
+      console.log("이러지 마세요");
+    }
+    /**
+     * clean up function
+     * useEffect 동작하기 전에 clean up function 내부의 코드부터 먼저 실행된다.
+     * 사용법: return ()=>{}
+     */
+    return () => {
+      //타이머 제거x
+      clearTimeout(a);
+    };
+    // useEffect 실행조건 넣을 수 있는 곳은 [실행조건](dependencies)
+    // []: Component가 최초 1회 mount 될 때만 실행됨(이후 실행 x)
+    // [count]: count라는 state가 변할 때 useEffect 실행됨
+  }, [count, input1]);
 
   // url parameter 사용한 것 받아와서 사용하기
   let { id } = useParams();
@@ -27,6 +46,18 @@ function Detail(props) {
   return (
     <div className="container">
       {alert === true ? <div className="alert alert-warning">2초이내 구매시 할인</div> : null}
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        count 상승 버튼
+      </button>
+      <input
+        onKeyUp={(e) => {
+          setInput1(e.target.value);
+        }}
+      />
       <div className="row">
         <div className="col-md-6">
           <img src={`https://codingapple1.github.io/shop/shoes1.jpg`} width="100%" alt="" />
